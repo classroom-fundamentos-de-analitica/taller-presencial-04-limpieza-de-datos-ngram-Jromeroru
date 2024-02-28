3007e1bc1ac0036035627475d56a3e11d7421eef
Joaquín Eduardo Romero Ruiz <159669386+Jromeroru@users.noreply.github.com>
2024-02-27 23:16:15 -0500

    4 clase

--- clean_data.py
+++ clean_data.py
@@ -6,9 +6,6 @@ import pandas as pd
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
 
-    #
-    # Esta parte es igual al taller anterior
-    #
     df = pd.read_csv(input_file)
     return df
 
@@ -17,30 +14,38 @@ def create_key(df, n):
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
 
     df = df.copy()
+
+    # Copie la columna 'text' a la columna 'key'
     df["key"] = df["text"]
+
+    # Remueva los espacios en blanco al principio y al final de la cadena
     df["key"] = df["key"].str.strip()
+
+    # Convierta el texto a minúsculas
     df["key"] = df["key"].str.lower()
+
+    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
     df["key"] = df["key"].str.replace("-", "")
+
+    # Remueva puntuación y caracteres de control
     df["key"] = df["key"].str.translate(
         str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
     )
+
+    # Convierta el texto a una lista de tokens
     df["key"] = df["key"].str.split()
 
-    # ------------------------------------------------------
-    # Esta es la parte especifica del algoritmo de n-gram:
-    #
-    # - Una el texto sin espacios en blanco
+    # Una el texto sin espacios en blanco
+    df["key"] = df["key"].str.join("")
+   
+    # Convierta el texto a una lista de n-gramas
+    df["key"] = df["key"].map(lambda x: [x[t:t+n-1] for t in range(len(x))])
 
-    #
-    # - Convierta el texto a una lista de n-gramas
+    # Ordene la lista de n-gramas y remueve duplicados
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
 
-    #
-    # - Ordene la lista de n-gramas y remueve duplicados
-
-    #
-    # - Convierta la lista de ngramas a una cadena
-
-    ## ------------------------------------------------------
+    # Convierta la lista de ngramas a una cadena
+    df["key"] = df["key"].str.join("")
 
     return df
 
@@ -48,13 +53,18 @@ def create_key(df, n):
 def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
-    #
-    # Este código es identico al anteior
-    #
     df = df.copy()
+
+    # Ordene el dataframe por 'key' y 'text'
     df = df.sort_values(by=["key", "text"], ascending=[True, True])
+
+    # Seleccione la primera fila de cada grupo de 'key'
     keys = df.drop_duplicates(subset="key", keep="first")
+
+    # Cree un diccionario con 'key' como clave y 'text' como valor
     key_dict = dict(zip(keys["key"], keys["text"]))
+
+    # Cree la columna 'cleaned' usando el diccionario
     df["cleaned"] = df["key"].map(key_dict)
 
     return df
@@ -62,9 +72,7 @@ def generate_cleaned_column(df):
 
 def save_data(df, output_file):
     """Guarda el DataFrame en un archivo"""
-    #
-    # Este código es identico al anteior
-    #
+
     df = df.copy()
     df = df[["cleaned"]]
     df = df.rename(columns={"cleaned": "text"})
@@ -73,9 +81,7 @@ def save_data(df, output_file):
 
 def main(input_file, output_file, n=2):
     """Ejecuta la limpieza de datos"""
-    #
-    # Este código es identico al anteior
-    #
+
     df = load_data(input_file)
     df = create_key(df, n)
     df = generate_cleaned_column(df)
